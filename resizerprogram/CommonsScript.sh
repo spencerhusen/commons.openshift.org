8a0ff93c6c2eba7fbe05fc985a6a5fa7c604b61e
Spencer Husen <spencerhusen@gmail.com>
2018-08-14 13:17:06 -0400

    Pushing new script that compiles Java program

--- resizerprogram/CommonsScript.sh
+++ resizerprogram/CommonsScript.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd $COMMONS_PATH/resizerprogram/src
+javac -cp commons/commons-io-2.6.jar  commons/ImageResizer.java
 curl -H "Authorization: token $GIT_TOKEN" https://api.github.com/repos/spencerhusen/commons.openshift.org/issues | java -cp .:commons/commons-io-2.6.jar commons.ImageResizer
